8c91cfed72b839c1f93a67cf6749867d0fe63732
zhanabil <zhanabil.sydykov@bcc.kz>
2024-06-03 14:36:34 +0500

    Add SlideAction settings

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,11 +12,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: SlideAction(
-        onSubmit: () {
-          // do something
-        },
-        ),
+      backgroundColor: Colors.blue[200],
+      body: Center(child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SlideAction(
+          borderRadius: 16,
+          elevation: 4,
+          innerColor: Colors.blue,
+          outerColor: Colors.blue[400],
+          sliderButtonIcon: const Icon(
+            Icons.double_arrow_rounded,
+            color: Colors.white,),
+          text: 'Slide to Unlock',
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+          sliderRotate: false,
+          onSubmit: () {
+            return null;
+          },
+          ),
+      ),
       ),
     );
   }
